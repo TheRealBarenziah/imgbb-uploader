865097385b4af2766e2647dc776bcdeb61e83125
Barenziah <lunasecrete@gmail.com>
2020-12-10 17:00:17 +0100

    wip

--- src/__tests__/imgbbUploaderRequire.test.js
+++ src/__tests__/imgbbUploaderRequire.test.js
@@ -9,3 +9,20 @@ test("imgbbUploader using require", async () => {
     ).then((res) => res.size),
   ).toBe(51895);
 });
+
+/*
+Todo: write four tests, 
+"using option object w/ 2 param", 
+"using option object w/ 3 param (expiration)",
+"using option object w/ 3 param (name)",
+"using option object w/ 4 params"
+
+expiration is passed into the query string
+name, in the stringified payload
+
+problem with test flow: once an image is uploaded, its 'signed' 
+and subsequents uploads of the same file will return the original response,
+which can lead to confusing results (aka the 'toto.jpg phenomenon')
+
+need to imagine something to generate a new picture every time..
+*/
