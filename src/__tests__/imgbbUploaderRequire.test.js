09012586a0c52c88266a77373266981eb4250636
TheRealBarenziah <presciente.snotrq@gmail.com>
2020-12-23 13:53:32 +0100

    wip: require test refacto

--- src/__tests__/imgbbUploaderRequire.test.js
+++ src/__tests__/imgbbUploaderRequire.test.js
@@ -1,18 +1,30 @@
+require("dotenv").config();
+
 const path = require("path");
+const imagePath = require("./images/imagePath");
 const imgbbUploader = require("../../lib/cjs");
+const generateWaifu = require("waifu-generator");
+const tfaker = require("tfaker");
 
 test("imgbbUploader using require", async () => {
+  const randomFilename = tfaker.firstName() + Date.now();
+  await generateWaifu({
+    path: "./src/__tests__/images",
+    filename: randomFilename,
+  });
   expect(
     await imgbbUploader(
       process.env.API_KEY,
-      path.join(__dirname, "megumin.jpg"),
-    ).then((res) => res.size),
-  ).toBe(51895);
+      path.join(imagePath, `${randomFilename}.jpg`),
+    ).then((res) => {
+      return res.title;
+    }),
+  ).toBe(randomFilename);
 });
 
 /*
-Todo: write four tests, 
-"using option object w/ 2 param", 
+Todo: write four tests,
+"using option object w/ 2 param",
 "using option object w/ 3 param (expiration)",
 "using option object w/ 3 param (name)",
 "using option object w/ 4 params"
@@ -20,18 +32,9 @@ Todo: write four tests,
 expiration is passed into the query string
 name, in the stringified payload
 
-problem with test flow: once an image is uploaded, its 'signed' 
+problem with test flow: once an image is uploaded, its 'signed'
 and subsequents uploads of the same file will return the original response,
 which can lead to confusing results (aka the 'toto.jpg phenomenon')
 
 need to imagine something to generate a new picture every time..
 */
-
-test("imgbbUploader using require", async () => {
-  expect(
-    await imgbbUploader(
-      process.env.API_KEY,
-      path.join(__dirname, "megumin.jpg"),
-    ).then((res) => res.size),
-  ).toBe(51895);
-});
