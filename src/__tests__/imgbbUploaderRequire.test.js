a040bd67c5ef47d5a8050db08d6667157491355e
TheRealBarenziah <presciente.snotrq@gmail.com>
2020-12-30 12:46:40 +0100

    wip: pass the whole test suite!

--- src/__tests__/imgbbUploaderRequire.test.js
+++ src/__tests__/imgbbUploaderRequire.test.js
@@ -94,7 +94,9 @@ test("imgbbUploader w/ require: passing an invalid option object should throw wh
     explosionPath: path.join(imagePath, `${randomFilename}.jpg`),
     apiKey: process.env.API_KEY,
   };
-  expect(await imgbbUploader(options).catch((e) => e)).toThrow();
+  return await imgbbUploader(options)
+    .then((res) => res)
+    .catch((e) => expect(e).toBeInstanceOf(Error));
 });
 
 test("imgbbUploader w/ require: passing an invalid option object should throw when 'apiKey' param is faulty", async () => {
@@ -104,19 +106,19 @@ test("imgbbUploader w/ require: passing an invalid option object should throw wh
     path: path.join(imagePath, `${randomFilename}.jpg`),
     naniKey: process.env.API_KEY,
   };
-  expect(
-    await imgbbUploader(options).then((res) => {
-      return {
-        name: res.image.name,
-        expiration: res.expiration,
-      };
-    }),
-  ).toThrow();
+  return await imgbbUploader(options)
+    .then((res) => res)
+    .catch((e) => expect(e).toBeInstanceOf(Error));
 });
 
-test("imgbbUploader w/ require: passing a 'good enough' option object should warn without throwing", async () => {
+test("imgbbUploader w/ require: passing a 'valid enough' option object should not throw", async () => {
   const valarDohaeris = tfaker.firstName();
   const randomFilename = valarDohaeris + Date.now();
+  const randomExpirationValue = Math.floor(Math.random() * 300) + 120;
+  await generateWaifu({
+    path: "./src/__tests__/images",
+    filename: randomFilename,
+  });
   const options = {
     imagePath: path.join(imagePath, `${randomFilename}.jpg`),
     apiKey: process.env.API_KEY,
@@ -129,55 +131,3 @@ test("imgbbUploader w/ require: passing a 'good enough' option object should war
     }),
   ).toBe(true);
 });
-
-/*
-Todo: write four tests,
-"using option object w/ 2 param",
-"using option object w/ 3 param (expiration)",
-"using option object w/ 3 param (name)",
-"using option object w/ 4 params"
-
-expiration is passed into the query string
-name, in the stringified payload
-
-problem with test flow: once an image is uploaded, its 'signed'
-and subsequents uploads of the same file will return the original response,
-which can lead to confusing results (aka the 'toto.jpg phenomenon')
-
-need to imagine something to generate a new picture every time..
-*/
-
-/*
-{
-        id: 'f2JV3RN',
-        title: 'ea5c61836e7a',
-        url_viewer: 'https://ibb.co/f2JV3RN',
-        url: 'https://i.ibb.co/rm9KBPp/ea5c61836e7a.jpg',
-        display_url: 'https://i.ibb.co/XbHfnB5/ea5c61836e7a.jpg',
-        size: 261642,
-        time: '1609157359',
-        expiration: '0',
-        image: {
-          filename: 'ea5c61836e7a.jpg',
-          name: 'ea5c61836e7a',
-          mime: 'image/jpeg',
-          extension: 'jpg',
-          url: 'https://i.ibb.co/rm9KBPp/ea5c61836e7a.jpg'
-        },
-        thumb: {
-          filename: 'ea5c61836e7a.jpg',
-          name: 'ea5c61836e7a',
-          mime: 'image/jpeg',
-          extension: 'jpg',
-          url: 'https://i.ibb.co/f2JV3RN/ea5c61836e7a.jpg'
-        },
-        medium: {
-          filename: 'ea5c61836e7a.jpg',
-          name: 'ea5c61836e7a',
-          mime: 'image/jpeg',
-          extension: 'jpg',
-          url: 'https://i.ibb.co/XbHfnB5/ea5c61836e7a.jpg'
-        },
-        delete_url: 'https://ibb.co/f2JV3RN/318b8e284dc9535ad91de38ef34ad19b'
-      }
-*/
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { rejects } from "assert";
 import { fileToString } from "./fileToString";
 import { postToImgbb } from "./postToImgbb";
 import { validateInput } from "./validateInput";
@@ -25,12 +26,16 @@ const imgbbUploader = async (...args: string[] | IOptions[]) => {
   // handle two string params to ensure retrocompatibility
   if (args.length === 2) {
     if (await validateInput(String(args[0]), String(args[1]))) {
-      return postToImgbb({
-        apiKey: String(args[0]),
-        base64str: await fileToString(String(args[1])),
-        name: null,
-        expiration: null,
-      });
+      try {
+        return postToImgbb({
+          apiKey: String(args[0]),
+          base64str: await fileToString(String(args[1])),
+          name: null,
+          expiration: null,
+        });
+      } catch (e) {
+        throw new Error(e);
+      }
     } else {
       throw new Error(
         "Invalid params: please make sure that first argument is an imgBB API key, and second argument is a valid path to image file.",
@@ -40,12 +45,16 @@ const imgbbUploader = async (...args: string[] | IOptions[]) => {
     const { imagePath, apiKey, name = null, expiration = null } = {
       ...args[0],
     };
-    return postToImgbb({
-      apiKey: String(apiKey),
-      base64str: await fileToString(String(imagePath)),
-      name: name ? String(name) : null,
-      expiration: expiration ? Number(expiration) : null,
-    });
+    try {
+      return postToImgbb({
+        apiKey: String(apiKey),
+        base64str: await fileToString(String(imagePath)),
+        name: name ? String(name) : null,
+        expiration: expiration ? Number(expiration) : null,
+      });
+    } catch (e) {
+      throw new Error(e);
+    }
   } else
     throw new Error(
       `It seems you didn't pass your arguments properly! Please check imgbbUploader documentation here:\nhttps://github.com/TheRealBarenziah/imgbb-uploader/tree/master`,
