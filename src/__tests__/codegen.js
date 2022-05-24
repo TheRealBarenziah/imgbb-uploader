5a885a41b1aef6b7eeab5720c19719cb7944de00
TheRealBarenziah <presciente.snotrq@gmail.com>
2022-05-24 13:29:13 +0200

    fix: condition in codegen.js (it now works as intended!)

--- src/__tests__/codegen.js
+++ src/__tests__/codegen.js
@@ -1,7 +1,6 @@
 const { writeFile } = require("fs/promises");
 const { resolve } = require("path");
 const { readdir, readFile } = require("fs").promises;
-const { createReadStream } = require("fs");
 const jestConfig = require("../../jestconfig.json");
 
 /*
@@ -35,13 +34,13 @@ const getFiles = async (dir) => {
   const testFiles = files.flat(2).filter((filepath) => {
     const filename = getFilenameFromPath(filepath);
     return (
-      filename.includes(".js") && !filesToIgnore.includes(filename) && !filepath.includes("export")
+      filename.includes(".js") && !filesToIgnore.includes(filename) && !filepath.includes("import")
     ); // whizbang++
   });
   return testFiles;
 };
 
-// Split string into an array of line & performs the relevant edits
+// Split string into an array of line then performs the relevant edits
 const editString = (jsString) => {
   const lines = jsString.split("\n");
   const editedLines = lines.map((jsLine) => {
@@ -64,7 +63,7 @@ const editString = (jsString) => {
 
         // Then take stuff to import
         const stuffToImport = jsLine.split("=")[0].replace("const", "import");
-        // Finally glue that together
+        // Finally, glue that together
         return `${stuffToImport}from ${path};`;
       }
     }
@@ -83,58 +82,25 @@ const codegen = async () => {
   // Promises must be kept preciously
   const jsStringsPromises = [];
 
-  let longestLoc = 0;
-
-  testFiles.forEach((file) =>
+  testFiles.forEach((file) => {
     jsStringsPromises.push(
       readFile(file, "utf-8")
         .then((string) => {
-          return editString(string);
-        })
-        .then((editedString) => {
+          const edited = editString(string);
           const pureFilename = getFilenameFromPath(file);
-          console.log("🐱 OBTW editedstring ?", `${editedString}`);
-
-          return writeFile(`${__dirname}/import/${pureFilename}`, editedString);
-        })
-        .then(async (res) => {
-          const pureFilename = getFilenameFromPath(file);
-          const newFilePath = `${__dirname}/import/${pureFilename}`;
-          function countFileLines(filePath) {
-            return new Promise((resolve, reject) => {
-              let lineCount = 0;
-              createReadStream(filePath)
-                .on("data", (buffer) => {
-                  let idx = -1;
-                  lineCount--; // Because the loop will run once for idx=-1
-                  do {
-                    idx = buffer.indexOf(10, idx + 1);
-                    lineCount++;
-                  } while (idx !== -1);
-                })
-                .on("end", () => {
-                  resolve(lineCount);
-                })
-                .on("error", reject);
-            });
-          }
-          const loc = await countFileLines(newFilePath);
-          console.log("LOC of new file: ", loc);
-          if (longestLoc < loc) longestLoc = loc;
-          return res;
+          return writeFile(`${__dirname}/import/${pureFilename}`, edited);
         })
         .catch((e) => {
           console.error("Problem in readFile.catch\n", e);
           throw e;
         }),
-    ),
-  );
+    );
+  });
 
   await Promise.all(jsStringsPromises).catch((e) => {
     console.error("Catch in promise.all", e);
     throw e;
   });
-  console.log("Done rewriting 'import' tests!\nLongest LOC : ", longestLoc);
   return null;
 };
 
